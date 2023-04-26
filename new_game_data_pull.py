f3441b9b1a082fd9b57e7cf9d1c42670722136f8
Eric Heiken <48847746+HgCNO2@users.noreply.github.com>
2023-04-25 22:34:36 -0400

    Sorting values by latest first.

--- new_game_data_pull.py
+++ new_game_data_pull.py
@@ -116,5 +116,6 @@ if not new_entries.empty:
             pass
         df.drop(columns='game_details', inplace=True)
         existing_table = pd.concat([existing_table, df])
+        existing_table.sort_values('game_year', ascending=False, inplace=True)
         existing_table.to_pickle('game_data.pickle.gz')
         browser.quit()
